c6a63bd30700eb72fcbc48e57827cd5b02084118
Ricardo Sanchez <ricsanfre@gmail.com>
2024-02-11 19:41:05 +0100

    Adding Springboot main class and banner and application.yaml

--- product-service/src/main/java/com/ricsanfre/microservices/core/product/Main.java
+++
@@ -1,7 +0,0 @@
-package com.ricsanfre.microservices.core.product;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ product-service/src/main/java/com/ricsanfre/microservices/core/product/ProductServiceApplication.java
@@ -0,0 +1,11 @@
+package com.ricsanfre.microservices.core.product;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+}
