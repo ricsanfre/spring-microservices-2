97452125e6c4f947008ed611d9c5f791a9164850
Ricardo Sanchez <ricsanfre@gmail.com>
2024-02-23 11:46:56 +0100

    Adding Recommendation microservice basic implementation

--- recommendation-service/src/main/java/com/ricsanfre/microservices/core/recommendation/RecommendationServiceApplication.java
+++ recommendation-service/src/main/java/com/ricsanfre/microservices/core/recommendation/RecommendationServiceApplication.java
@@ -3,7 +3,11 @@ package com.ricsanfre.microservices.core.recommendation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.ricsanfre.microservices"
+        }
+)
 public class RecommendationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RecommendationServiceApplication.class, args);
