4dd4d41fd6cbd0763e6f37ace7d99f71c113bcae
Ricardo Sanchez <ricsanfre@gmail.com>
2024-02-23 10:29:11 +0100

    Adding API library containing backend microservices DTO and API interfaces definition

---
+++ product-service/src/test/java/com/ricsanfre/microservices/core/product/ProductServiceApplicationTest.java
@@ -0,0 +1,7 @@
+package com.ricsanfre.microservices.core.product;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceApplicationTest {
+
+}
---
+++ product-service/src/test/java/com/ricsanfre/microservices/core/product/ProductServiceApplicationTests.java
@@ -0,0 +1,8 @@
+package com.ricsanfre.microservices.core.product;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductServiceApplicationTests {
+
+}
