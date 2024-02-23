79019b62ec7cd6ee5191242c7d0d71fcf920e087
Ricardo Sanchez <ricsanfre@gmail.com>
2024-02-23 11:53:06 +0100

    Adding Review microservice basic implementation

--- review-service/src/main/java/com/ricsanfre/microservices/core/review/ReviewServiceApplication.java
+++ review-service/src/main/java/com/ricsanfre/microservices/core/review/ReviewServiceApplication.java
@@ -3,7 +3,11 @@ package com.ricsanfre.microservices.core.review;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.ricsanfre.microservices"
+        }
+)
 public class ReviewServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReviewServiceApplication.class, args);
