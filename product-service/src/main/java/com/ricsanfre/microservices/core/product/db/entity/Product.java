d0af86cd40e1b0af2dfc7fb642c5898e11260a24
Ricardo Sanchez <ricsanfre@gmail.com>
2024-04-13 12:41:59 +0200

    Adding lombok to product microservice

--- product-service/src/main/java/com/ricsanfre/microservices/core/product/db/entity/Product.java
+++ product-service/src/main/java/com/ricsanfre/microservices/core/product/db/entity/Product.java
@@ -1,5 +1,7 @@
 package com.ricsanfre.microservices.core.product.db.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -8,6 +10,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document (
         collection = "products"
 )
+@Data
+@NoArgsConstructor
 public class Product {
 
     @Id
@@ -22,52 +26,10 @@ public class Product {
     private String name;
     private int weight;
 
-    public Product() {
-    }
-
     public Product(int productId, String name, int weight) {
         this.productId = productId;
         this.name = name;
         this.weight = weight;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Integer getVersion() {
-        return version;
-    }
-
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
-
-    public int getProductId() {
-        return productId;
-    }
-
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
 }
